bbccaec673a970dfb6eaf679f683e1fab89a87f8
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2024-02-10 21:36:18 +0700

    init nuxt

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button class="rounded bg-blue-500 text-white px-4 py-2" @click="createMatch">Create a new game</button>
+    <button class="rounded bg-blue-500 text-white px-4 py-2" @click="joinMatch">Join existing game</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const createMatch = () => {
+  console.log('createMatch')
+}
+const joinMatch = () => {
+  console.log('joinMatch')
+}
+</script>
