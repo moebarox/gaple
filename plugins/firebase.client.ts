492b8057f7e7f146c7ba221589cd2d863df9ebad
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2024-03-17 18:46:02 +0700

    setup appConfig

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 export default defineNuxtPlugin(() => {
+  const { firebase } = useAppConfig()
   const firebaseConfig = {
-    apiKey: 'AIzaSyC4GHM4TMz8Bcg0BfbWpEOQyPF8JEgq3Z8',
-    authDomain: 'gaple-a66b9.firebaseapp.com',
-    projectId: 'gaple-a66b9',
-    storageBucket: 'gaple-a66b9.appspot.com',
-    messagingSenderId: '341027833404',
-    appId: '1:341027833404:web:b1d2062967dfe876c66539',
-    measurementId: 'G-TWVCYMGNQX',
+    apiKey: firebase.apiKey,
+    authDomain: firebase.authDomain,
+    projectId: firebase.projectId,
+    storageBucket: firebase.storageBucket,
+    messagingSenderId: firebase.messagingSenderId,
+    appId: firebase.appId,
+    measurementId: firebase.measurementId,
   }
 
   const app = initializeApp(firebaseConfig)
